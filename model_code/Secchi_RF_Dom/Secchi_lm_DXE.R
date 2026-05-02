3d72dffbea7d02dd0756857d2ef13771c1e4d0c2
Dominique Edwards <edwardsd9@newpaltz.edu>
2026-05-02 11:30:16 -0400

    fixes to automation

--- model_code/Secchi_RF_Dom/Secchi_lm_DXE.R
+++ model_code/Secchi_RF_Dom/Secchi_lm_DXE.R
@@ -1,4 +1,3 @@
-
 # 1. Load the required packages
 library(tidyverse)
 library(lubridate)
@@ -8,15 +7,24 @@ library(vera4castHelpers)
 my_forecast_date <- Sys.Date() - lubridate::days(1)
 my_model_id <- 'Secchi_lm_Dom'
 
-# 3. Source the math function from your R folder
+# 3. Source the math function from  R folder
 source('./R/Secchi_Dom/Secchi_LM_model_code.R')
 
 # 4. Run the function!
 final_forecast <- Dom_LM_Secchi_model_function(forecast_date = my_forecast_date, model_id = my_model_id)
 
-# save and submit
-write.csv(final_forecast, './model_output/secchi_DXE/Secchi_df.csv')
+# ---------------------------------------------------------
+# SAVE AND SUBMIT BLOCK
+# ---------------------------------------------------------
 
-vera4castHelpers::forecast_output_validator('./model_output/secchi_DXE/Secchi_df.csv')
+# Step A: Guarantee the folder exists before trying to save anything
+dir.create("model_output/secchi_DXE", showWarnings = FALSE, recursive = TRUE)
 
-vera4castHelpers::submit('./model_output/secchi_DXE/Secchi_df.csv', s3_region = "submit", s3_endpoint = "ltreb-reservoirs.org", first_submission = FALSE)
+# Step B: Save the file
+write.csv(final_forecast, "model_output/secchi_DXE/Secchi_df.csv", row.names = FALSE)
+
+# Step C: Validate the file 
+vera4castHelpers::forecast_output_validator("model_output/secchi_DXE/Secchi_df.csv")
+
+# Step D: Submit the file 
+vera4castHelpers::submit("model_output/secchi_DXE/Secchi_df.csv", s3_region = "submit", s3_endpoint = "ltreb-reservoirs.org", first_submission = FALSE)
--- model_code/Secchi_RF_Dom/Secchi_rf_DXE.R
+++ model_code/Secchi_RF_Dom/Secchi_rf_DXE.R
@@ -1,4 +1,3 @@
-
 # 1. Load the required packages
 library(tidyverse)
 library(lubridate)
@@ -6,7 +5,7 @@ library(vera4castHelpers)
 
 # 2. Set arguments
 my_forecast_date <- Sys.Date() - lubridate::days(1)
-my_model_id <- 'Secchi_rf_Dom'
+my_model_id <- 'Dom_RF_Secchi' 
 
 # 3. Source the math function from your R folder
 source('./R/Secchi_Dom/Secchi_RF_Model_Code.R')
@@ -14,9 +13,18 @@ source('./R/Secchi_Dom/Secchi_RF_Model_Code.R')
 # 4. Run the function!
 final_forecast <- Dom_RF_Secchi_model_function(forecast_date = my_forecast_date, model_id = my_model_id)
 
-# save and submit
-write.csv(final_forecast, './model_output/secchi_DXE/Secchi_rf_df.csv')
+# ---------------------------------------------------------
+# SAVE AND SUBMIT BLOCK
+# ---------------------------------------------------------
 
-vera4castHelpers::forecast_output_validator('./model_output/secchi_DXE/Secchi_rf_df.csv')
+# Step A: Guarantee the folder exists BEFORE trying to save anything
+dir.create("model_output/secchi_DXE", showWarnings = FALSE, recursive = TRUE)
 
-vera4castHelpers::submit('./model_output/secchi_DXE/Secchi_rf_df.csv', s3_region = "submit", s3_endpoint = "ltreb-reservoirs.org", first_submission = FALSE)
+# Step B: Save the file using the clean path (no leading ./)
+write.csv(final_forecast, "model_output/secchi_DXE/Secchi_rf_df.csv", row.names = FALSE)
+
+# Step C: Validate the file using the clean path
+vera4castHelpers::forecast_output_validator("model_output/secchi_DXE/Secchi_rf_df.csv")
+
+# Step D: Submit the file using the clean path
+vera4castHelpers::submit("model_output/secchi_DXE/Secchi_rf_df.csv", s3_region = "submit", s3_endpoint = "ltreb-reservoirs.org", first_submission = FALSE)
